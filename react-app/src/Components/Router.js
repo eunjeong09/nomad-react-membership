c25db91c2c7ab0c296b217a38d7fb4cf9e76614c
eunjeong09 <ejko0911@gmail.com>
2021-04-01 23:46:36 +0900

    Style react react프로젝트에 스타일 생성

--- react-app/src/Components/Router.js
+++ react-app/src/Components/Router.js
@@ -3,14 +3,20 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-d
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Header from './Header'
+
+
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" component={TV} />
-      <Route path="/search" component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
