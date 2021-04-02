157b3fdae4d91ff4fea45db20c84dcc78cf4fb6e
eunjeong09 <ejko0911@gmail.com>
2021-04-03 05:00:26 +0900

    Add folder, index, container, presenter 컴포넌트별로 폴더를 만들고, 폴더 안에 index, container, presenter작성

--- react-app/src/Components/Router.js
+++ react-app/src/Components/Router.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import Home from "../Routes/Home";
-import TV from "../Routes/TV";
-import Search from "../Routes/Search";
+import TV from "../Routes/TV/index";
+import Search from "../Routes/Search/index";
 import Header from './Header'
 
 
